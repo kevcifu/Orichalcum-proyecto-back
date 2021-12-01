3e595e9e6858f3536613477766e4799eabb005c1
KarenLRuizM <karenloruiz2309@gmail.com>
2021-11-30 22:04:46 -0500

    Anadí modelo proyectos

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const UserModel = require('./models/usuario');
+const { ProjectsModel } = require('./models/proyectos');
 const conectarBD = require('./db/db');
 
 const main = async () => {
     await conectarBD();
+    await ProjectsModel.create({
+        nombre: "Proyecto1",
+        presupuesto: 500000000,
+        fechaInicial: Date.now(),
+        fechaFinal: Date.now(),
+        objetivoGeneral: "prueba1",
+        objetivoEspecifico: "pruebapp",
+        lider: "61a6d87275ea06462a5d707d",
+
+    
+    }).then((p) =>{ 
+        console.log("proyecto creado", p );
+    }).catch((e)=>{
+        console.error("error", e );
+    }); 
 };
 
 main();
