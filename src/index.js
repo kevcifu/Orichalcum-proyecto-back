aba3f68230d4ac86974d4b9fa9a189ce3bbc6e08
Sebastian Mejia <deadbygravity@gmail.com>
2021-12-01 21:56:00 -0500

    Pruebas de creación de documentos avance e inscripciones

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const { ProjectsModel } = require('../src/models/proyectos');
 const { UserModel } = require('../src/models/usuario');
+const {InscriptionModel} = require('../src/models/inscripcion');
+const {AdvanceModel} = require('../src/models/avances');
 const conectarBD = require('../db/db');
 
 const main = async () => {
@@ -32,6 +34,30 @@ const main = async () => {
     }).then((u)=>{
         console.log(u);
     }).catch((e) => {
+        console.error(e);
+    });
+
+    await InscriptionModel.create({
+        proyecto: '61a6d87275ea06462a5d808d',
+        estudiante: '61a6d87275ea06462a5d606f',
+        estado: 'INSCRITO',
+        fechaIncripcion: Date.now(),
+        fechaEgreso: Date.now()
+
+    }).then((i)=>{
+        console.log(i);
+    }).catch((e)=>{
+        console.error(e);
+    });
+
+    await AdvanceModel.create({
+        proyecto: '61a6d87275ea06462a5d808d',
+        fecha: Date.now(),
+        descripcion: 'avance 1',
+        observaciones: 'observacion avance 1'
+    }).then((a)=>{
+        console.log(a);
+    }).catch((e)=>{
         console.error(e);
     });
 
