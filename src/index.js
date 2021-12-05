83eb3fb827619b31a9f40c58075d62a56ac6d8c8
Sebastian Mejia <deadbygravity@gmail.com>
2021-12-04 19:54:24 -0500

    Se creó la función getDatosUsuario para validar el token y se creó el servidor con ApolloServer además de otros detalles de corrección

--- src/index.js
+++ src/index.js
@@ -1,10 +1,51 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const conectarBD = require('../db/db');
+import express, { json } from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { ApolloServer } from 'apollo-server-express';
+import { conectarBD } from './db/db.js';
+import { validarToken } from './token/token';
+import { TypeInfo } from 'graphql';
 
-const main = async () => {
-    await conectarBD();
+dotenv.config();
 
+const getDatosUsuario = (token) =>{
+    const verificacion = validarToken(token.split(' ')[1]);
+    if (verificacion.data) {
+        return verificacion.data;
+    } else {
+        return null;
+    }
 };
 
-main();
+const servidor = new ApolloServer({
+    typeDefs: tipo,
+    resolvers: resolver,
+    context: ({req, res}) => {
+        const token = req.headers?.authorization ?? null;
+        if (token) {
+            const datosUsuario = getDatosUsuario(token);
+            if (datosUsuario) {
+                return {datosUsuario};
+            }
+        }
+        return null;
+    },
+});
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.listen({ port: 4000}, async ()=>{
+    await conectarBD();
+    await servidor.start();
+
+    servidor.applyMiddleware({ app });
+    
+    console.log(`servidor corriendo`)
+});
+
